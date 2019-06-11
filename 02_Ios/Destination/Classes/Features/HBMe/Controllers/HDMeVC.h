917bb66685b63053d3143d3e564bdd1da403264b
hubo <hallohubo@gmail.com>
2019-06-11 11:07:08 +0800

    add mine 模块

---
+++ 02_Ios/Destination/Classes/Features/HBMe/Controllers/HDMeVC.h
@@ -0,0 +1,17 @@
+//
+//  HDMeVC.h
+//  Destination
+//
+//  Created by 胡勃 on 6/11/19.
+//  Copyright © 2019 Redirect. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HDMeVC : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ 02_Ios/Destination/Classes/Features/HBMe/Controllers/HDMeVC.m
@@ -0,0 +1,49 @@
+//
+//  HDMeVC.m
+//  Destination
+//
+//  Created by 胡勃 on 6/11/19.
+//  Copyright © 2019 Redirect. All rights reserved.
+//
+
+#import "HDMeVC.h"
+
+@interface HDMeVC ()
+{
+    IBOutlet UIImageView        *imvHeadBackground;
+    IBOutlet UIImageView        *imvHeadPerson;
+    IBOutlet UIImageView        *imvOpenMemberArrow;
+    IBOutlet UIButton           *btnOpenMember;//开通会员
+    IBOutlet UIButton           *btnRecharge;//充值
+    IBOutlet UIButton           *btnWithdrawal;//提现
+    IBOutlet UILabel            *lbName;
+    IBOutlet UILabel            *lbCommission;//佣金余额
+    IBOutlet UILabel            *lbTask;//任务余额
+    IBOutlet UILabel            *lbDeposit;//保证金
+    IBOutlet UILabel            *lbFrozen;//冻结余额
+    IBOutlet UIButton           *btnIncome;//收支明细
+    IBOutlet UIButton           *btnShare;//分享得佣金
+    IBOutlet UIButton           *btnCutomer;//联系客服
+    IBOutlet UIButton           *btnSetting;//系统设置
+}
+
+@end
+
+@implementation HDMeVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
