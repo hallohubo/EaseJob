9556d808f2a35c050cc666a8ba13ac64c87ee456
hubo <hallohubo@gmail.com>
2019-06-27 11:09:35 +0800

    DiscoverVC small modify

--- 02_Ios/Destination/Classes/Features/Discover/Controllers/HDDiscoverVC.m
+++ 02_Ios/Destination/Classes/Features/Discover/Controllers/HDDiscoverVC.m
@@ -29,14 +29,16 @@
 {
     IBOutlet UITextField    *tfSearch;
     IBOutlet UIView         *vSearch;
-    IBOutlet UITableView    *tbv;
+    IBOutlet UIScrollView    *tbv;
     IBOutlet SPPageMenu     *vPageMenu;
+    IBOutlet UIView         *vHead;
     NSArray                     *arBtn;
     NSURLSessionDataTask        *task;
     NSMutableArray              *marDiscoverList;
 }
 
 @property (nonatomic, strong) NSArray *dataArr;
+@property (nonatomic, strong) NSMutableArray *myChildViewControllers;
 
 @end
 
@@ -141,11 +143,48 @@
                     return;
                 }
                 marDiscoverList = object;
-                [tbv reloadData];
+               
                 
             }];
 }
 
+#pragma mark - scrollViewDelegate
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
+    
+     //这一步是实现跟踪器时刻跟随scrollView滑动的效果,如果对self.pageMenu.scrollView赋了值，这一步可省
+//     [vPageMenu moveTrackerFollowScrollView:scrollView];
+}
+
+#pragma mark - SPPageMenu的代理方法
+
+- (void)pageMenu:(SPPageMenu *)pageMenu itemSelectedAtIndex:(NSInteger)index {
+    NSLog(@"%zd",index);
+}
+
+- (void)pageMenu:(SPPageMenu *)pageMenu itemSelectedFromIndex:(NSInteger)fromIndex toIndex:(NSInteger)toIndex {
+    NSLog(@"%zd------->%zd",fromIndex,toIndex);
+    
+    // 如果该代理方法是由拖拽self.scrollView而触发，说明self.scrollView已经在用户手指的拖拽下而发生偏移，此时不需要再用代码去设置偏移量，否则在跟踪模式为SPPageMenuTrackerFollowingModeHalf的情况下，滑到屏幕一半时会有闪跳现象。闪跳是因为外界设置的scrollView偏移和用户拖拽产生冲突
+    if (!tbv.isDragging) { // 判断用户是否在拖拽scrollView
+        // 如果fromIndex与toIndex之差大于等于2,说明跨界面移动了,此时不动画.
+        if (labs(toIndex - fromIndex) >= 2) {
+            [tbv setContentOffset:CGPointMake(kSCREEN_WIDTH * toIndex, 0) animated:NO];
+        } else {
+            [tbv setContentOffset:CGPointMake(kSCREEN_WIDTH * toIndex, 0) animated:YES];
+        }
+    }
+    
+    if (self.myChildViewControllers.count <= toIndex) {return;}
+    
+    UIViewController *targetViewController = self.myChildViewControllers[toIndex];
+    // 如果已经加载过，就不再加载
+    if ([targetViewController isViewLoaded]) return;
+    
+    targetViewController.view.frame = CGRectMake(kSCREEN_WIDTH * toIndex, 0, kSCREEN_WIDTH, tbv.bounds.size.height);
+    [tbv addSubview:targetViewController.view];
+    
+}
 
 #pragma mark - event
 
@@ -162,20 +201,70 @@
 
 - (void)setupPageMenuInit
 {
-    self.dataArr = @[@"生活",@"影视中心",@"交通",@"电视剧",@"搞笑",@"综艺"];
-    
-    // trackerStyle:跟踪器的样式
+    self.dataArr = @[@"全部",@"简单",@"高价",@"UIP"];
+    //UIView *window = [UIApplication sharedApplication].keyWindow;
     CGRect frame = [vPageMenu convertRect:vPageMenu.bounds toView:self.view];
-    Dlog(@"WINDowframe:%@", NSStringFromCGRect(frame));
+//    Dlog(@"WINDowframe0:%@", NSStringFromCGRect(window.frame));
+//    Dlog(@"WINDowbound0:%@", NSStringFromCGRect(window.bounds));
+
+    Dlog(@"WINDowframe1:%@", NSStringFromCGRect(frame));
+//    frame.origin.y = frame.origin.y-20;
     
-    vPageMenu = [SPPageMenu pageMenuWithFrame:frame trackerStyle:SPPageMenuTrackerStyleLineAttachment];
-    // 传递数组，默认选中第2个
-    [vPageMenu setItems:self.dataArr selectedItemIndex:1];
+    vPageMenu = [SPPageMenu pageMenuWithFrame:frame trackerStyle:SPPageMenuTrackerStyleLine];
+    
+    // 传递数组，默认选中第1个
+    [vPageMenu setItems:self.dataArr selectedItemIndex:0];
+//    vPageMenu.backgroundColor = HDCOLOR_RED;
     // 设置代理
     vPageMenu.delegate = self;
-    // 给pageMenu传递外界的大scrollView，内部监听self.scrollView的滚动，从而实现让跟踪器跟随self.scrollView移动的效果
+    //给pageMenu传递外界的大scrollView，内部监听self.scrollView的滚动，
+    //从而实现让跟踪器跟随self.scrollView移动的效果
     vPageMenu.bridgeScrollView = tbv;
     [self.view addSubview:vPageMenu];
     
+    NSArray *controllerClassNames = [NSArray arrayWithObjects:@"FirstViewController",@"SecondViewController",@"ThidViewController", @"FourViewController", nil];
+    for (int i = 0; i < self.dataArr.count; i++) {
+        if (controllerClassNames.count > i) {
+            BaseViewController *baseVc = [[NSClassFromString(controllerClassNames[i]) alloc] init];
+            id object = [vPageMenu contentForItemAtIndex:i];
+            if ([object isKindOfClass:[NSString class]]) {
+                baseVc.text = object;
+            } else if ([object isKindOfClass:[UIImage class]]) {
+                baseVc.text = @"图片";
+            } else {
+                SPPageMenuButtonItem *item = (SPPageMenuButtonItem *)object;
+                baseVc.text = item.title;
+            }
+            [self addChildViewController:baseVc];
+            // 控制器本来自带childViewControllers,但是遗憾的是该数组的元素顺序永远无法改变，只要是addChildViewController,都是添加到最后一个，而控制器不像数组那样，可以插入或删除任意位置，所以这里自己定义可变数组，以便插入(删除)(如果没有插入(删除)功能，直接用自带的childViewControllers即可)
+            [self.myChildViewControllers addObject:baseVc];
+        }
+    }
+    
+    // pageMenu.selectedItemIndex就是选中的item下标
+    if (vPageMenu.selectedItemIndex < self.myChildViewControllers.count) {
+        BaseViewController *baseVc = self.myChildViewControllers[vPageMenu.selectedItemIndex];
+        CGRect frame = [tbv convertRect:tbv.bounds toView:self.view];
+        [tbv addSubview:baseVc.view];
+        
+        baseVc.view.frame = CGRectMake(kSCREEN_WIDTH * vPageMenu.selectedItemIndex, 0, kSCREEN_WIDTH, tbv.bounds.size.height);
+        
+        tbv.contentOffset = CGPointMake(kSCREEN_WIDTH * vPageMenu.selectedItemIndex, 0);
+        tbv.contentSize = CGSizeMake(self.dataArr.count * kSCREEN_WIDTH, 0);
+    }
+    
+}
+
+- (NSMutableArray *)myChildViewControllers {
+    
+    if (!_myChildViewControllers) {
+        _myChildViewControllers = [NSMutableArray array];
+        
+    }
+    return _myChildViewControllers;
+}
+
+- (void)dealloc {
+    NSLog(@"父控制器被销毁了");
 }
 @end
